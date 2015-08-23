f55878977ddf7413b23050d686bd76b40d99b685
Tomche Delev <tomce.delev@gmail.com>
2015-08-23 16:39:48 +0200

    Improve styling and show version number

--- d/app.js
+++ d/app.js
@@ -40,6 +40,17 @@
   'use strict';
   angular
     .module('taximk')
+    .controller('Main', Main);
+
+  function Main(version) {
+    var vm = this;
+    vm.version = version;
+  }
+})();
+(function () {
+  'use strict';
+  angular
+    .module('taximk')
     .controller('Places', Places);
 
   function Places(places) {
